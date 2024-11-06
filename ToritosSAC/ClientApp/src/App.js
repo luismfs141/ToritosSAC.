2ef248057cba53e8983944f6d82b28ecd06f1fc5
Roy <r.vargas.carrasco@isur.edu.pe>
2024-11-06 00:26:20 -0500

    cambios prueba

--- ToritosSAC/ClientApp/src/App.js
+++ ToritosSAC/ClientApp/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 const App = () => {
     return (
         <div classname="Container">
-               <h1>Hola a todos</h1>
+               <h1>Hola a todos amigos</h1>
         </div>
         )
 }
